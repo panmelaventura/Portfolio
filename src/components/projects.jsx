73681728d71f6e82393cbaf7ebcf5500d3bb4e30
panmelaventura <panmelaventura@gmail.com>
2018-09-17 22:23:47 -0400

    versao final 17/09

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -7,13 +7,12 @@ import anchor from './anchor.jpg';
 import bannerLi from './bannerLi.jpg';
 import cartaoLi from './cartaoLi.jpg';
 import cardapioLi from './cardapioLi.jpg';
-// import folder from './folder.jpg';
-// import catalogo from './catalogo.jpg';
-// import novo from './cartaoTersel.jpg';
-// import manu from './manu.jpg';
-// import nana from './nana.jpg';
-// import festa from './festa.jpg';
-
+import folder from './folder.jpg';
+import catalogo from './catalogo.jpg';
+import cartaoTersel from './cartaoTersel.jpg';
+import manu from './manu.png';
+import nana from './nana.jpg';
+import festa from './festa.jpg';
 
 
 class Projects extends Component {
@@ -50,52 +49,54 @@ class Projects extends Component {
             return (
                 <div className="projects-grid">
                 {/* <h3 className='h3-style' style={{ marginBottom:'500px'}}>Visual Identity</h3> */}
-                    <Card shadow={0} style={{width: '270px', height: '380px', margin: 'auto', marginRight: '20px', marginBottom: '10px'}}>
-                    <img src={bannerLi} style={{ height: '100%', padding:'0'}} />
+
+                    <Card shadow={0} style={{width: '270px', height: '380px', margin: 'auto', marginRight: '5px', marginBottom: '15px'}}>
+                    <img src={folder} alt={"folder"} style={{ height: '100%', padding:'0'}} />
                     </Card>
 
-                    <Card shadow={0} style={{width: '304', height: '346', margin: 'auto', marginRight: '20px', marginBottom: '10px'}}>
+                    <Card shadow={0} style={{width: '304px', height: '380px', margin: 'auto', marginRight: '5px', marginBottom: '15px'}}>
+                    <img src={cartaoTersel} alt={"cartaoTersel"} style={{ height: '100%', padding:'0'}} />
+                    </Card>
+
+                    <Card shadow={0} style={{width: '270px', height: '380px', margin: 'auto', marginRight: '5px', marginBottom: '15px'}}>
+                    <img src={catalogo} alt={"catalogo"} style={{ height: '100%', padding:'0'}} />
+                    </Card>
+
+                    
+
+
+                    <Card shadow={0} style={{width: '270px', height: '380px', margin: 'auto', marginRight: '5px', marginBottom: '15px'}}>
+                    <img src={cardapioLi} style={{ height: '100%', padding:'0'}} />
+                    </Card>
+
+                    <Card shadow={0} style={{width: '304px', height: '380px', margin: 'auto', marginRight: '5px', marginBottom: '15px'}}>
                     <img src={cartaoLi} style={{ height: '100%', padding:'0'}} />
                     </Card>
 
-                    <Card shadow={0} style={{width: '270px', height: '380px', margin: 'auto', marginRight: '20px', marginBottom: '10px'}}>
-                    <img src={cardapioLi} style={{ height: '100%', padding:'0'}} />
+                    <Card shadow={0} style={{width: '270px', height: '380px', margin: 'auto', marginRight: '5px',  marginBottom: '15px'}}>
+                    <img src={bannerLi} alt={"bannerLi"} style={{ height: '100%', padding:'0'}} />
                     </Card>
 
 
-
-                    <Card shadow={0} style={{width: '270', height: '380px', margin: 'auto', marginRight: '20px', marginBottom: '10px'}}>
-                    <a href="components/catalogo.pdf" download style={{ height: '100%', padding:'0'}}><img src={cardapioLi}  /></a>
+                    {/* <Card shadow={0} style={{width: '270px', height: '380px', margin: 'auto', marginRight: '5px', marginBottom: '15px'}}>
+                    <img src={manu} style={{ height: '100%', padding:'0'}} />
                     </Card>
 
-                    <Card shadow={0} style={{width: '270px', height: '380px', margin: 'auto', marginRight: '20px', marginBottom: '10px'}}>
-                    <img src={cardapioLi} style={{ height: '100%', padding:'0'}} />
+                    <Card shadow={0} style={{width: '304px', height: '380px', margin: 'auto', marginRight: '5px', marginBottom: '15px'}}>
+                    <img src={festa} style={{ height: '100%', padding:'0'}} />
                     </Card>
 
-                    <Card shadow={0} style={{width: '270px', height: '380px', margin: 'auto', marginRight: '20px', marginBottom: '10px'}}>
-                    <img src={cardapioLi} style={{ height: '100%', padding:'0'}} />
-                    </Card>
+                    <Card shadow={0} style={{width: '270px', height: '380px', margin: 'auto', marginRight: '5px', marginBottom: '15px'}}>
+                    <img src={manu} style={{ height: '100%', padding:'0'}} />
+                    </Card> */}
+
+                   
+                    
 
 
 
-
-                    <Card shadow={0} style={{width: '270px', height: '380px', margin: 'auto', marginRight: '20px', marginBottom: '10px'}}>
-                    <img src={cardapioLi} style={{ height: '100%', padding:'0'}} />
-                    </Card>
-
-                    <Card shadow={0} style={{width: '270px', height: '380px', margin: 'auto', marginRight: '20px', marginBottom: '10px'}}>
-                    <img src={cardapioLi} style={{ height: '100%', padding:'0'}} />
-                    </Card>
-
-                    <Card shadow={0} style={{width: '256px', height: '256px', background: 'url(components/manu.jpg) center / cover', margin: 'auto'}}>
-                        <CardTitle expand />
-                        <CardActions style={{height: '52px', padding: '16px', background: 'rgba(0,0,0,0.2)'}}>
-                            <span style={{color: '#fff', fontSize: '14px', fontWeight: '500'}}>
-                                manu.jpg
-                            </span>
-                        </CardActions>
-                    </Card>
-
+                    
+                    
                 </div>
             )
         
