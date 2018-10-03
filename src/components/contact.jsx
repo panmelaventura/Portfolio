f3078eddfda9ab08e5c1c4c5656140660bcbcaa7
panmelaventura <panmelaventura@gmail.com>
2018-10-03 17:52:15 -0400

    link adjustment

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -13,35 +13,35 @@ class Contact extends Component {
                     <div className="contact-list" style={{textAlign: 'center'}}> 
                     <List>
                     <ListItem> 
-                        <ListItemContent style={{fontSize:'20px'}}>
+                        <ListItemContent style={{fontSize:'18px'}}>
                             <i className="fa fa-phone" aria-hidden="true"/>
                             (416) 938 6402
                         </ListItemContent>
                     </ListItem>
 
                     <ListItem>
-                        <ListItemContent style={{fontSize:'20px'}}>
+                        <ListItemContent style={{fontSize:'18px'}}>
                             <i className="fa fa-envelope" aria-hidden="true"/>
                             panmelaventura@gmail.com
                         </ListItemContent>
                     </ListItem>
 
                      <ListItem>
-                        <ListItemContent style={{fontSize:'20px'}}>
+                        <ListItemContent style={{fontSize:'18px'}}>
                             <i className="fa fa-skype" aria-hidden="true"/>
                             live:panmelaventura
                         </ListItemContent>
                     </ListItem>
 
                      <ListItem>
-                        <ListItemContent style={{fontSize:'20px'}}>
+                        <ListItemContent style={{fontSize:'18px'}}>
                             <i className="fa fa-github" aria-hidden="true"/>
                             <a href="https://www.github.com/panmelaventura" target="_blank" style={{textDecoration: 'none', color:'black'}} >GitHub</a>
                         </ListItemContent>
                      </ListItem>
 
                      <ListItem>
-                        <ListItemContent style={{fontSize:'20px'}}>
+                        <ListItemContent style={{fontSize:'18px'}}>
                             <i className="fa fa-linkedin" aria-hidden="true"/>
                             <a href="https://www.linkedin.com/in/panmela-ventura-da-graca-de-morais-275878164/" target="_blank" style={{textDecoration: 'none', color:'black'}} >Panmela Morais</a>
                         </ListItemContent> 
--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -4,19 +4,11 @@ import snackbar from './snackbar.jpg';
 import '../App.css';
 import makeup from './makeup.jpg';
 import arcomaquinas from './arcomaquinas.jpg';
-import anchor from './anchor.jpg';
-import bannerLi from './bannerLi.jpg';
-import cartaoLi from './cartaoLi.jpg';
-import cardapioLi from './cardapioLi.jpg';
 import folder from './folder.jpg';
 import catalogo from './catalogo.jpg';
 import cartaoTersel from './cartaoTersel.jpg';
-import manu from './manu.png';
-import nana from './nana.jpg';
-import festa from './festa.jpg';
-import FolderTersel from './pdf/Folder-Tersel-.pdf';
-import Scheuch from './pdf/Scheuch-Tersel.pdf';
-
+import folderTersel from './pdf/Folder-Tersel-.pdf';
+import scheuch from './pdf/Tersel.pdf';
 
 class Projects extends Component {
     constructor(props) {
@@ -27,13 +19,9 @@ class Projects extends Component {
     toggleCategories() {
         if (this.state.activeTab === 1) {
             return (
-
                 <div className="projects-grid">
-
                     <Card shadow={0} style={{ width: '270px', height: '380px', margin: 'auto', marginRight: '5px', marginBottom: '10px' }}>
-                        {/* <a href={snackbar} target="_blank">  */}
-                            <img src={snackbar} alt={"snackbar"} style={{ height: '100%',   padding: '0' }}  />
-                        {/* </a>  */}
+                        <img src={snackbar} alt={"snackbar"} style={{ height: '100%', padding: '0' }} />
                     </Card>
 
                     <Card shadow={0} style={{ width: '270px', height: '380px', margin: 'auto', marginRight: '5px', marginBottom: '10px' }}>
@@ -47,21 +35,13 @@ class Projects extends Component {
                             <img src={arcomaquinas} alt={"arcomaquinas"} style={{ height: '100%', width: '100%', padding: '0' }} />
                         </a>
                     </Card>
-
                 </div>
             )
-
-            {/*<Card shadow={0} style={{width: '270px', height: '380px', margin: 'auto', marginBottom: '10px',  marginRight:'370px'}}>
-            <img src={anchor} alt={"anchor"} style={{ height: '100%', padding:'0'}} />           
-            </Card>  */}
-
         } else if (this.state.activeTab === 0) {
             return (
                 <div className="projects-grid">
-                    {/* <h3 className='h3-style' style={{ marginBottom:'500px'}}>Visual Identity</h3> */}
-
                     <Card shadow={0} className="projectCard">
-                        <a href={FolderTersel} target="_blank">
+                        <a href={folderTersel} target="_blank">
                             <img src={folder} alt={"folder"} style={{ height: '100%', width: '100%', padding: '0', border: '0' }} />
                         </a>
                     </Card>
@@ -73,18 +53,14 @@ class Projects extends Component {
                     </Card>
 
                     <Card shadow={0} className="projectCard">
-                        <a href={Scheuch} target="_blank">
+                        <a href={scheuch} target="_blank">
                             <img src={catalogo} alt={"catalogo"} style={{ height: '100%', width: '100%', padding: '0' }} />
                         </a>
                     </Card>
-
                 </div>
             )
-
         }
     }
-
-
 
     render() {
         return (
@@ -104,5 +80,5 @@ class Projects extends Component {
         )
     }
 }
-export default Projects;
 
+export default Projects;
